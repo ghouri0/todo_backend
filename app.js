d2ab197e9efc5f79a0ccf3b11591281ae7c26ed9
ghouri0 <alimuhammadhghouri0@gmail.com>
2023-02-12 11:25:11 +0500

    project Files added

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const appRouter = require("./routes/todoRoute.js");
-const path = require('path')
+const path = require("path");
 
-app.use(cors({
-  origin: '*'
-}));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -17,8 +23,7 @@ mongoose.connect(
   "mongodb+srv://aliGhouri:Alighouri@cluster0.fo9cxyz.mongodb.net/test"
 );
 
-app.use('/todo', appRouter)
-
+app.use("/todo", appRouter);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
