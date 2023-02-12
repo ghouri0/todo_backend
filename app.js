d95c9ea9dc10fbe0dd1b939987c4cebd3126780b
ghouri0 <alimuhammadhghouri0@gmail.com>
2023-02-12 19:31:41 +0500

    Project added

--- app.js
+++ app.js
@@ -7,14 +7,10 @@ import bodyParser from "body-parser";
 import appRouter from "./routes/todoRoute.js";
 
 app.use(cors());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
